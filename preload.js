776babbbc588b7e5facc15de031698d3d16d948f
Roko <rokovukovic191@gmail.com>
2026-03-18 14:11:18 +0100

    Add getCanInterfaces

--- preload.js
+++ preload.js
@@ -1,11 +1,13 @@
-const { contextBridge, ipcRenderer } = require('electron');
 
+const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
     connectCAN: (iface) => ipcRenderer.invoke('connect-can', iface),
     disconnectCAN: () => ipcRenderer.invoke('disconnect-can'),
+    getCanInterfaces: () => ipcRenderer.invoke('get-can-interfaces'),
     onCANData: (callback) => ipcRenderer.on('can-data', (_event, payload) => callback(payload)),
     showSaveDialog: () => ipcRenderer.invoke('show-save-dialog'),
     saveCSV: (filePath, data) => ipcRenderer.invoke('save-csv', filePath, data),
     onCANStatus: (callback) => ipcRenderer.on('can-status', (_event, payload) => callback(payload)),
     onCANEvents: (callback) => ipcRenderer.on('can-events', (_event, payload) => callback(payload))
 });
+ 
